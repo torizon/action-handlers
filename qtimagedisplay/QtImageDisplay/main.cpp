eb60b59abd1a754c2a43751b4d321178553d94f0
Drew Moseley <drew@moseleynet.net>
2024-02-22 16:15:14 -0500

    qtimagedisplay: Initial display for splashscreen subsystem updates
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

---
+++ qtimagedisplay/QtImageDisplay/main.cpp
@@ -0,0 +1,22 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url("qrc:/QtImageDisplay/QML/main.qml");
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+
+    engine.load(url);
+
+    std::cout << "Hello Torizon!" << std::endl;
+
+    return app.exec();
+}
---
+++ qtimagedisplay/README.md
@@ -0,0 +1,7 @@
+# QT Image Display Update Example
+
+In this example, a PNG file is used as the payload and ultimately copied to the `/home/torizon` directory on the target.
+
+This example uses a container in the drewmoseley/ namespace.  The sources to rebuild this application are provided in the QtImageDisplay directory.
+
+For a full overview of this example, check this [Toradex Article](https://developer.toradex.com/torizon/torizon-platform/torizon-updates/how-to-use-subsystem-updates/).
